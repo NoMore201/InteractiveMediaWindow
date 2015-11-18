1b5c2c59515f424ccb7061c0f604c897486cbefe
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2015-11-18 16:33:02 +0100

    added class that lets log data to the console or to a file

---
+++ DataLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    class DataLog
+    {
+        public void ToConsole(String message)
+        {
+            Debugger.Log(1, "INTERACTIVEMEDIAWINDOW: ", message);
+        }
+
+        public void ToFile(String message)
+        {
+            StreamWriter file = new StreamWriter(
+                "..\\..\\..\\DataLogs.txt", 
+                File.Exists("..\\..\\..\\DataLogs.txt"
+            ));
+
+            file.WriteLine("[" + DateTime.Now.ToString() + "]" + message);
+
+            file.Close();
+        }
+    }
+}
